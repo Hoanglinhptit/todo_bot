6039b89b7602e9aec8b9b0b463f0e66d407e7bf9
hoanglinh <hoanglinh@vnpt.vn>
2024-05-14 14:27:11 +0700

    bot say done

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -2,21 +2,13 @@ package com.example.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.telegram.telegrambots.meta.TelegramBotsApi
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+
 import org.telegram.telegrambots.starter.EnableTelegramBot;
 
 @SpringBootApplication
 @EnableTelegramBot
 class DemoApplication {
 	static void main(String[] args) {
-//			try {
-//				TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class)
-//				botsApi.registerBot(new TelegramBot())
 				SpringApplication.run(DemoApplication, args)
-//			}catch (TelegramApiException e){
-//				e.printStackTrace()
-//			}
 	}
 }
--- src/main/groovy/entities/TaskEntity.groovy
+++ src/main/groovy/entities/TaskEntity.groovy
@@ -1,53 +1,51 @@
-//package entities
-//
-//import groovy.transform.builder.Builder
-//import jakarta.persistence.Entity
-//import jakarta.persistence.GeneratedValue
-//import jakarta.persistence.GenerationType
-//import jakarta.persistence.Id
-//import jakarta.persistence.Table
-//
-//@Entity
-//@Builder
-//@Table(name = "task")
-//class TaskEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private Long userId;
-//    private String text;
-//    private boolean completed;
-//
-//    // getters and setters
-//    Long getId() {
-//        return id
-//    }
-//
-//    void setId(Long id) {
-//        this.id = id
-//    }
-//
-//    Long getUserId() {
-//        return userId
-//    }
-//
-//    void setUserId(Long userId) {
-//        this.userId = userId
-//    }
-//
-//    String getText() {
-//        return text
-//    }
-//
-//    void setText(String text) {
-//        this.text = text
-//    }
-//
-//    boolean getCompleted() {
-//        return completed
-//    }
-//
-//    void setCompleted(boolean completed) {
-//        this.completed = completed
-//    }
-//}
+package entities
+
+import groovy.transform.builder.Builder
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+@Builder
+class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long chatId;
+    private String text;
+    private boolean completed;
+
+    // getters and setters
+    Long getId() {
+        return id
+    }
+
+    void setId(Long id) {
+        this.id = id
+    }
+
+    Long getChatId() {
+        return chatId
+    }
+
+    void setChatId(Long chatId) {
+        this.chatId = chatId
+    }
+
+    String getText() {
+        return text
+    }
+
+    void setText(String text) {
+        this.text = text
+    }
+
+    boolean getCompleted() {
+        return completed
+    }
+
+    void setCompleted(boolean completed) {
+        this.completed = completed
+    }
+}
